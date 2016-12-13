3a7ae1af02edcb8bdc326f07ce12c123d7b5fe2b
raghav <raghav@watchy.in>
2016-12-13 22:14:32 +0530

    slightly better code because it handles the row and col movements after each row or col is done. there is one bug for 2 tests m=10,n=1 m=1,n=10 where the array is printed twice.

--- programming/arrays/spiralordermatrix.cc
+++ programming/arrays/spiralordermatrix.cc
@@ -8,55 +8,53 @@ vector<int> spiralOrder(const vector<vector<int> > &A) {
     // DO STUFF HERE AND POPULATE result
     int m = A.size();
     int n = A[0].size();
-    cout << "debug: " << "m = " << m << " n= " << n << endl;
+    //cout << "debug: " << "m = " << m << " n= " << n << endl;
         
     int nElems = m * n;
-    int cnt = 0;
+    //int cnt = 0;
     int trow = 0, brow = m - 1, lcol = 0, rcol = n - 1;
     
-    while (cnt != nElems) {
+    //while (cnt != nElems) {
+    while (trow <= brow && lcol <= rcol) {
+
+        //cout << "debug: " << " before " << "trow=" << trow << " lcol=" << lcol << " brow=" << brow << " rcol=" << rcol << endl;
+
         // print left->right
         for (int i = lcol; i <= rcol; ++i) {
             //cout << A[trow][i] << "\t";
             result.push_back(A[trow][i]);
         }
         
+        ++trow;
+        
         // print top->bot
-        for (int i = trow + 1; i <= brow; ++i) {
+        for (int i = trow; i <= brow; ++i) {
             //cout << A[i][rcol] << "\t";
             result.push_back(A[i][rcol]);
         }
         
+        --rcol;
+        
         // print bot right->bot left
-        for (int i = rcol - 1; i >= lcol; --i) {
+        for (int i = rcol; i >= lcol; --i) {
             //cout << A[brow][i] << "\t";
             result.push_back(A[brow][i]);
         }
         
+        --brow;
+        
         // print bot left->top
-        for (int i = brow - 1; i > trow; --i) {
+        for (int i = brow; i >= trow; --i) {
             //cout << A[i][lcol] << "\t";
             result.push_back(A[i][lcol]);
         }
         
-        cnt += (rcol - lcol + 1) + (brow - trow) + (rcol - lcol) + (brow - trow - 1);
-        
-        cout << endl;
-        cout << "debug: " << "trow=" << trow << " lcol=" << lcol << " brow=" << brow << " rcol=" << rcol;
-        cout << "debug: " << "cnt=" << cnt << endl;
-        
-        ++trow;
-        --brow;
         ++lcol;
-        --rcol;
+        
+        //cout << "debug: " << " after " << "trow=" << trow << " lcol=" << lcol << " brow=" << brow << " rcol=" << rcol << endl;
     }
     
     return result;
-    // Do not write main() function.
-    // Do not read input, instead use the arguments to the function.
-    // Do not print the output, instead return values as specified
-    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
-  
 }
 
 vector<vector<int> > getMatrix(int m, int n)
@@ -65,7 +63,7 @@ vector<vector<int> > getMatrix(int m, int n)
     for (int i = 0; i < m; ++i) {
         vector<int> row;
         for (int j = 0; j < n; ++j) {
-            row.push_back(i*10+j);
+            row.push_back(i*10 + j*10 + j);
         }
         
         A.push_back(row);
@@ -77,15 +75,18 @@ vector<vector<int> > getMatrix(int m, int n)
 
 void printMatrix(vector<vector<int> >&A)
 {
+    cout << endl << "Matrix m= " << A.size() << " n=" << A[0].size() << endl;
     for (int i = 0; i < A.size(); ++i) {
         for (int j = 0; j < A[i].size(); ++j)
             cout << A[i][j] << "\t";
         cout << endl;
     }
+    cout << endl;
 }
 
 void printVector(vector<int>& A)
 {
+    cout << "debug: " << "num elements in the vector=" << A.size() << endl;
     for (int i = 0; i < A.size(); ++i)
         cout << A[i] << "\t";
     cout << endl;
@@ -93,27 +94,20 @@ void printVector(vector<int>& A)
 
 int main(int argc, char *argv[])
 {
-    vector<vector<int> > A = getMatrix(4,4);
+    int m[] = {4, 4, 2, 3, 1, 10, 3, 10, 1};
+    int n[] = {4, 2, 4, 2, 1, 2, 1, 1, 5};
+    
+    vector<vector<int> > A;
     vector<int> result;
     
-    printMatrix(A);
-    result = spiralOrder(A);
-    printVector(result);
+    int len = (int)(sizeof(m)/sizeof(int));
     
-    A = getMatrix(4,2);
-    printMatrix(A);
-    result = spiralOrder(A);
-    printVector(result);
-    
-    A = getMatrix(2,4);
-    printMatrix(A);
-    result = spiralOrder(A);
-    printVector(result);
-    
-    //A = getMatrix(1,1);
-    //printMatrix(A);
-    //result = spiralOrder(A);
-    //printVector(result);
+    for (int i = 0; i < len; ++i) {
+        A = getMatrix(m[i], n[i]);
+        printMatrix(A);
+        result = spiralOrder(A);
+        printVector(result);
+    }
     
     return 0;
 }
